c281f71447ff809a7571be6a836c3c4baf46baba
Imon Ovbude <imonovbude@gmail.com>
2019-06-24 13:29:23 -0700

    setup routes to use views/layouts

--- index.js
+++ index.js
@@ -1,36 +1,49 @@
+const path = require("path");
+const expressEdge = require("express-edge");
 const express = require("express");
 
-const path = require("path");
 const app = express();
 
 app.use(express.static("public"));
 
 // Use Express Edge
-// app.use(require("express-edge"));
-// app.set("views", `${__dirname}/views`);
+app.use(expressEdge);
+app.set("views", `${__dirname}/views`);
 
 // Home Page Route
 app.get("/", (req, res) => {
   //Express Static
-  res.sendFile(path.resolve(__dirname, "pages/index.html"));
+  // res.sendFile(path.resolve(__dirname, "pages/index.html"));
 
   // Express Edge
-  // res.render("index");
+  res.render("index");
 });
 
 // About Page Route
 app.get("/about", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "pages/about.html"));
+  // Express Static
+  // res.sendFile(path.resolve(__dirname, "pages/about.html"));
+
+  // Express Edge
+  res.render("about");
 });
 
 // Contact Page Route
 app.get("/contact", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "pages/contact.html"));
+  // Express Static
+  // res.sendFile(path.resolve(__dirname, "pages/contact.html"));
+
+  // Express Edge
+  res.render("contact");
 });
 
 // Post Page Route
 app.get("/posts", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "pages/post.html"));
+  // Express Static
+  // res.sendFile(path.resolve(__dirname, "pages/post.html"));
+
+  // Express Edge
+  res.render("post");
 });
 
 const port = 3000;
