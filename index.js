8edf5a6da0c973fb6f6ab20925c50c7e57bd7d12
Imon Ovbude <imonovbude@gmail.com>
2019-06-24 12:26:21 -0700

    created get routes for all pages

--- index.js
+++ index.js
@@ -5,8 +5,32 @@ const app = express();
 
 app.use(express.static("public"));
 
+// Use Express Edge
+// app.use(require("express-edge"));
+// app.set("views", `${__dirname}/views`);
+
+// Home Page Route
 app.get("/", (req, res) => {
+  //Express Static
   res.sendFile(path.resolve(__dirname, "pages/index.html"));
+
+  // Express Edge
+  // res.render("index");
+});
+
+// About Page Route
+app.get("/about", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "pages/about.html"));
+});
+
+// Contact Page Route
+app.get("/contact", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "pages/contact.html"));
+});
+
+// Post Page Route
+app.get("/posts", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "pages/post.html"));
 });
 
 const port = 3000;
