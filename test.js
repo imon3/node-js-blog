ceb6938cd45715551c338c15bffda51bf474a686
Imon Ovbude <imonovbude@gmail.com>
2019-06-24 17:04:52 -0700

    added test for the Post database in test.js

--- test.js
+++ test.js
@@ -17,6 +17,22 @@ mongoose.connect("mongodb://localhost/node-js-test-blog");
 // );
 
 // find all records
-Post.find({}, (err, post) => {
-  console.log(err, post);
-});
+// Post.find({}, (err, post) => {
+//   console.log(err, post);
+// });
+
+// find record by id
+// Post.findById("5d114fdd442f6226f0baccf5", (err, post) => {
+//   console.log(err, post);
+// });
+
+// find record by id and update
+Post.findByIdAndUpdate(
+  "5d114fdd442f6226f0baccf5",
+  {
+    title: "My First Blog Post Title Lorem Ipsum"
+  },
+  (err, post) => {
+    console.log(err, post);
+  }
+);
